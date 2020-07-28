382cb3f72b31c00130492333c14512935c0b51ae
wuqian <wuqian.li@united-imaging.com>
2020-07-28 15:25:00 +0800

    first

---
+++ Project2/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "header.h"
+
+int main (int agc, char* argv[])
+{
+    std::cout << "hello world" << std::endl;
+    foo();
+    return 0;
+}
---
+++ Project3/fun/source.cpp
@@ -0,0 +1,6 @@
+#include "header.h"
+#include <iostream>
+void foo()
+{
+    std::cout << "this is foo" << std::endl;
+}
