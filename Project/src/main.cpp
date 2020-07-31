cf0344e651893d1ce15ff39c46d6a369d572402e
wuqian <wuqian.li@united-imaging.com>
2020-07-31 10:58:41 +0800

    add boost and program_options

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
 #include "header.h"
 
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+
+namespace po = boost::program_options;
+using namespace std;
+
 int main (int agc, char* argv[])
 {
+	// Declare the supported options.
+	po::options_description desc("Allowed options");
+	desc.add_options()
+		("help", "produce help message")
+		("compression", po::value<int>(), "set compression level")
+	;
+
+	po::variables_map vm;
+	po::store(po::parse_command_line(agc, argv, desc), vm);
+	po::notify(vm);    
+
+    if (0 == vm.size())
+	{
+		std::cout << desc << std::endl;
+		return -1;
+	}
+	
+	if (vm.count("help")) {
+		cout << desc << "\n";
+		return 1;
+	}
+
+	if (vm.count("compression")) {
+		cout << "Compression level was set to " 
+	 << vm["compression"].as<int>() << ".\n";
+	} else {
+		cout << "Compression level was not set.\n";
+	}
+	
     std::cout << "hello world" << std::endl;
     foo();
     return 0;
